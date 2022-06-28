17ee49745f28813a668936124f4cd6c089623b6f
Rok Ragnarok <yoekzumaya@gmail.com>
2022-06-27 19:25:22 -0500

    a

--- src/Components/Character/List.js
+++ src/Components/Character/List.js
@@ -1,23 +1,33 @@
-import React, {useState, useEffect} from 'react';
+ 
+import React,  {useState, useEffect} from 'react';
 import Character from './Character';
+import Pagination from '@mui/material/Pagination';
+import { Link, MemoryRouter, Route, Routes, useLocation } from 'react-router-dom';
+import PaginationItem from '@mui/material/PaginationItem';
 
+import Stack from '@mui/material/Stack';
+import { Typography } from '@mui/material';
 export default function List() {
     const [characters, setCharacter] = useState([]);
     const [loading, setLoading] = useState(true);
-    
-const [currentPageUrl, setCurrentPageUrl] = useState("https://rickandmortyapi.com/api/character");
-const [nextPageUrl, setNextPageUrl] = useState();
-const[prevPageUrl, setPrevPageUrl] = useState();
-const [pages, setPages] = useState ();
-
+    const [currentPageUrl, setCurrentPageUrl] = useState("https://rickandmortyapi.com/api/character");
+    const [nextPageUrl, setNextPageUrl] = useState();
+    const [prevPageUrl, setPrevPageUrl] = useState();
+    const [pages, setPages] = useState();
+    const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  const page = parseInt(query.get('pages') || '1', 42);
     useEffect(() => {
-        const  url = currentPageUrl;
+        const url = currentPageUrl;
+
         async function fetchData() {
             const data = await fetch(url);
             const {results, info} = await data.json();
+
             setCharacter(results);
-            setNextPageUrl(info.next);
             setLoading(false);
+
+            setNextPageUrl(info.next);
             setPrevPageUrl(info.prev);
             setPages(info.pages);
         }
@@ -25,36 +35,85 @@ const [pages, setPages] = useState ();
         fetchData();
     }, [currentPageUrl]);
 
-    const nextPage = () =>{
+    //Next Page
+    const nextPage = () => {
         setCurrentPageUrl(nextPageUrl);
     }
-const prevPage = () =>{
-    setCurrentPageUrl(prevPageUrl);
-} 
 
-const goToPage = (num) =>{
-    setCurrentPageUrl(`https://rickandmortyapi.com/api/character?page=${num}`);
-}
+    //Prev Page
+    const prevPage = () => {
+        setCurrentPageUrl(prevPageUrl);
+    }
+
+    //Choose Page
+    const goToPage= (num) => {
+        setCurrentPageUrl(`https://rickandmortyapi.com/api/character?page=${num}`);
+    }
+
+    if (loading)
+        return (<div>Loading...</div>);
+
     return (
+        
         <div>
+            
             <h2>Characters</h2>
             <div className="row">
+                
+                
+                <Pagination 
+                 count={42}
+                 color = "secondary"
+                    nextPage={nextPageUrl ? nextPage : null}
+                    prevPage={prevPageUrl ? prevPage : null}
+                    goToPage={goToPage}
+                    pages={pages}
+                    
+                    renderItem={(item) => (
+                        <PaginationItem
+                          component={Link}
+                          to={`/inbox${item.page === 1 ? '' :  `?page=${item.page}`}`}
+                          {...item}
+                          
+                        />
+
+
+                        
+                      )}
+                    
+                />
+                
+                
+                
+                
+                
+              
+
                 {
-                    loading?(
-                     <div>loading...</div>
-                    )
-                    :
-                   ( 
-                characters.map((character) => (
-                    <Character
-                        key={character.id}
-                        name={character.name}
-                        origin={character.origin}
-                        image={character.image}
-                    />
-                )))
+                    characters.map((character) => (
+                        <Character
+                            key={character.id}
+                            name={character.name}
+                            origin={character.origin}
+                            image={character.image}
+                        />
+                    ))
                 }
+
+           
+<Pagination count ={42}
+color = "primary"
+            
+                    nextPage={nextPageUrl ? nextPage : null}
+                    prevPage={prevPageUrl ? prevPage : null}
+                    goToPage={goToPage}
+                    pages={pages}
+                    
+            
+                    
+                />
+                
             </div>
         </div>
-    )
+    );
 }
--- src/Components/Character/Pagination.js
+++ src/Components/Character/Pagination.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
-function Pagination({nextPage,prevPage,goToPage, pages}) {
+function Pagination({nextPage, prevPage, goToPage, pages}) {
 
-    let pageButtons =  [];
-    for (let i = 1; i <= pages; i++) {
-        pageButtons.push(<button key={i} 
-        onclick ={()=> goToPage(i)}>
-            {i}
-            </button>)
+    let pageButtons = [];
+    for (let i = 1; i <= pages; i++){
+        pageButtons.push(<button 
+                            key={i} 
+                            onClick={() => goToPage(i)}>
+                                {i}
+                        </button>)
     }
+
     return (
         <div>
-            {prevPage && (<button onclick= {prevPage}>◄</button>)}
+            {prevPage && (<button onClick= {prevPage}>◄</button>)}
             {pageButtons}
-            {nextPage && (<button onclick= {nextPage}>►</button>)}
+            {nextPage && (<button onClick= {nextPage}>►</button>)}
         </div>
     );
 }
